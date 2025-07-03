848ec732572adaa7b25fa76b4be88af492868509
Joe Pattenden <151946049+JojojoMohojo@users.noreply.github.com>
2025-07-03 20:09:37 +0100

    Deploy to gh-pages

--- src/virtual-list.js
+++
@@ -1,9 +0,0 @@
-export class VirtualList{
-    constructor(title, todos, dateHint = null) {
-        this.id = `virtual-${title.toLowerCase()}`;
-        this.title = title;
-        this.todos = todos;
-        this.dateHint = dateHint ? new Date(dateHint) : null;
-        this.isVirtual = true;
-    }
-}
